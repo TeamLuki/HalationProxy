502472219df85e5209a66accdcd800f5dce8fefd
Atmatm6 <atmatm6000@gmail.com>
2017-10-09 16:15:05 -0600

    removed websockets
    going to choose normal net sockets instead

--- index.js
+++ index.js
@@ -5,24 +5,6 @@ var mcserver = nmp.createServer({
     host: host,
     port: port
 });
-var WebSocket = require("ws");
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database("db");
-var server = new require("http").createServer();
-var wsvr = new WebSocket.Server({
-    server: server
-});
-wsvr.on("connection", function (ws) {
-    ws.on("message", (rawd) => {
-        var data = JSON.parse(rawd);
-        switch (data.name) {
-            case "login":
-                break;
-        }
-    });
-});
-server.listen({
-    host: host,
-    port: port
-});
 mcserver.listen(port, host);
